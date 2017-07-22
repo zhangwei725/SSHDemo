f3fc0f0836bc8776cd486d51258975b5d11636c6
zhangwei725 <zw520725>
2017-07-22 23:59:40 +0800

    添加logback将错误信息保存到数据库

--- src/main/java/com/werner/webapp/base/HibernateHandler.java
+++
@@ -1,9 +0,0 @@
-package com.werner.webapp.base;
-
-
-import javax.persistence.EntityManager;
-import java.io.Serializable;
-
-public abstract interface HibernateHandler extends Serializable {
-    public abstract Object doInJPA(EntityManager entityManager);
-}  
